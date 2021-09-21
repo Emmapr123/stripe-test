7fe2e8244bcfff547995ce4f07c23b52bfaf09e7
emmapriester@hotmail.com <emmapriester@hotmail.com>
2021-09-21 13:05:27 +0100

    Creditcard input and google pay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Creditcardinput from './components/creditcard-input/creditcard-input';
+import Googlepay from './components/google-pay/google-pay';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Creditcardinput/>
+      {/* <Googlepay /> */}
     </div>
   );
 }
