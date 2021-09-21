a4841db135830b1ad79ee84e980ee00f7419ea4e
emmapriester@hotmail.com <emmapriester@hotmail.com>
2021-09-21 16:57:22 +0100

    Slight cleanup and add Readme

--- src/App.js
+++ src/App.js
@@ -80,11 +80,11 @@ const ErrorMessage = ({ children }) => (
   <div className="ErrorMessage" role="alert">
     <svg width="16" height="16" viewBox="0 0 17 17">
       <path
-        fill="#FFF"
+        fill="black"
         d="M8.5,17 C3.80557963,17 0,13.1944204 0,8.5 C0,3.80557963 3.80557963,0 8.5,0 C13.1944204,0 17,3.80557963 17,8.5 C17,13.1944204 13.1944204,17 8.5,17 Z"
       />
       <path
-        fill="#6772e5"
+        fill="black"
         d="M8.5,7.29791847 L6.12604076,4.92395924 C5.79409512,4.59201359 5.25590488,4.59201359 4.92395924,4.92395924 C4.59201359,5.25590488 4.59201359,5.79409512 4.92395924,6.12604076 L7.29791847,8.5 L4.92395924,10.8739592 C4.59201359,11.2059049 4.59201359,11.7440951 4.92395924,12.0760408 C5.25590488,12.4079864 5.79409512,12.4079864 6.12604076,12.0760408 L8.5,9.70208153 L10.8739592,12.0760408 C11.2059049,12.4079864 11.7440951,12.4079864 12.0760408,12.0760408 C12.4079864,11.7440951 12.4079864,11.2059049 12.0760408,10.8739592 L9.70208153,8.5 L12.0760408,6.12604076 C12.4079864,5.79409512 12.4079864,5.25590488 12.0760408,4.92395924 C11.7440951,4.59201359 11.2059049,4.59201359 10.8739592,4.92395924 L8.5,7.29791847 L8.5,7.29791847 Z"
       />
     </svg>
@@ -120,8 +120,6 @@ const CheckoutForm = () => {
     event.preventDefault();
 
     if (!stripe || !elements) {
-      // Stripe.js has not loaded yet. Make sure to disable
-      // form submission until Stripe.js has loaded.
       return;
     }
 
@@ -166,8 +164,7 @@ const CheckoutForm = () => {
         Payment successful
       </div>
       <div className="ResultMessage">
-        Thanks for trying Stripe Elements. No money was charged, but we
-        generated a PaymentMethod: {paymentMethod.id}
+         {paymentMethod.id}
       </div>
       <ResetButton onClick={reset} />
     </div>
@@ -175,7 +172,6 @@ const CheckoutForm = () => {
     <form className="Form" onSubmit={handleSubmit}>
       <fieldset className="FormGroup">
         <Field
-          // label="Name"
           id="name"
           type="text"
           placeholder="Name"
@@ -187,7 +183,6 @@ const CheckoutForm = () => {
           }}
         />
         <Field
-          // label="Email"
           id="email"
           type="email"
           placeholder="email"
@@ -199,7 +194,6 @@ const CheckoutForm = () => {
           }}
         />
         <Field
-          // label="Phone"
           id="phone"
           type="tel"
           placeholder="Phone number"
@@ -235,8 +229,6 @@ const ELEMENTS_OPTIONS = {
   ]
 };
 
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
 const stripePromise = loadStripe("pk_test_6pRNASCoBOKtIshFeQd4XMUh");
 
 const App = () => {
